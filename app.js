6a253b80b824c16acbf6db220392e77cc012cced
viviane-rezende <vivianerezende95@gmail.com>
2024-08-16 09:49:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var soma = require("./operacoes/soma")
+var sub = require("./operacoes/sub")
+var multi = require("./operacoes/multi")
+var divi = require("./operacoes/divi")
+
+console.log(soma(10,10))
+console.log(sub(20,5))
+console.log(multi(5,5))
+console.log(divi(8,4))
